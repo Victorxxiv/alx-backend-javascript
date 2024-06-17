1144f9f7e320762389cc50a4abc1e4cc5e9c450c
Victor Mwendwa <victormwendwa804@gmail.com>
2024-06-17 07:26:54 +0000

    More mapping

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,15 +1,7 @@
-// 0-get_list_students.js
-
-/**
-* Function to get a list of students.
-* @returns {Array} Array of student objects.
-*/
-function getListStudents() {
+export default function getListStudents;
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' }
   ];
 }
-
-export default getListStudents;
